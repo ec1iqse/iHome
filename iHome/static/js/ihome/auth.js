7619ffa5499bcc6962e882df43129224b4b3d539
User <admin@admin.com>
2020-03-19 23:31:35 +0800

    图片验证码完成

--- iHome/static/js/ihome/auth.js
+++ iHome/static/js/ihome/auth.js
@@ -1,8 +1,9 @@
 function showSuccessMsg() {
-    $('.popup_con').fadeIn('fast', function() {
-        setTimeout(function(){
-            $('.popup_con').fadeOut('fast',function(){}); 
-        },1000) 
+    $('.popup_con').fadeIn('fast', function () {
+        setTimeout(function () {
+            $('.popup_con').fadeOut('fast', function () {
+            });
+        }, 1000)
     });
 }
 
--- iHome/static/js/ihome/register.js
+++ iHome/static/js/ihome/register.js
@@ -1,87 +1,95 @@
 function getCookie(name) {
-    var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
+    let r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
     return r ? r[1] : undefined;
 }
 
-var imageCodeId = "";
+//保存图片验证码ID
+let imageCodeId = "";
 
 function generateUUID() {
-    var d = new Date().getTime();
-    if(window.performance && typeof window.performance.now === "function"){
+    let d = new Date().getTime();
+    if (window.performance && typeof window.performance.now === "function") {
         d += performance.now(); //use high-precision timer if available
     }
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = (d + Math.random()*16)%16 | 0;
-        d = Math.floor(d/16);
-        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        let r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
     });
     return uuid;
 }
 
 function generateImageCode() {
+    // 形成图片验证码的后端地址，设置到页面中，让浏览器请求验证码图片
+    // 生成图片验证码编号
+    imageCodeId = generateUUID();
+    let url = "/api/v1.0/image_codes/" + imageCodeId;
+    //设置图片url
+    $(".image-code img").attr("src", url);
+
+
 }
 
 function sendSMSCode() {
     $(".phonecode-a").removeAttr("onclick");
-    var mobile = $("#mobile").val();
+    let mobile = $("#mobile").val();
     if (!mobile) {
         $("#mobile-err span").html("请填写正确的手机号！");
         $("#mobile-err").show();
         $(".phonecode-a").attr("onclick", "sendSMSCode();");
         return;
-    } 
-    var imageCode = $("#imagecode").val();
+    }
+    let imageCode = $("#imagecode").val();
     if (!imageCode) {
         $("#image-code-err span").html("请填写验证码！");
         $("#image-code-err").show();
         $(".phonecode-a").attr("onclick", "sendSMSCode();");
         return;
     }
-    $.get("/api/smscode", {mobile:mobile, code:imageCode, codeId:imageCodeId}, 
-        function(data){
+    $.get("/api/smscode", {mobile: mobile, code: imageCode, codeId: imageCodeId},
+        function (data) {
             if (0 != data.errno) {
-                $("#image-code-err span").html(data.errmsg); 
+                $("#image-code-err span").html(data.errmsg);
                 $("#image-code-err").show();
                 if (2 == data.errno || 3 == data.errno) {
                     generateImageCode();
                 }
                 $(".phonecode-a").attr("onclick", "sendSMSCode();");
-            }   
-            else {
-                var $time = $(".phonecode-a");
-                var duration = 60;
-                var intervalid = setInterval(function(){
-                    $time.html(duration + "秒"); 
-                    if(duration === 1){
+            } else {
+                let $time = $(".phonecode-a");
+                let duration = 60;
+                let intervalid = setInterval(function () {
+                    $time.html(duration + "秒");
+                    if (duration === 1) {
                         clearInterval(intervalid);
-                        $time.html('获取验证码'); 
+                        $time.html('获取验证码');
                         $(".phonecode-a").attr("onclick", "sendSMSCode();");
                     }
                     duration = duration - 1;
-                }, 1000, 60); 
+                }, 1000, 60);
             }
-    }, 'json'); 
+        }, 'json');
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     generateImageCode();
-    $("#mobile").focus(function(){
+    $("#mobile").focus(function () {
         $("#mobile-err").hide();
     });
-    $("#imagecode").focus(function(){
+    $("#imagecode").focus(function () {
         $("#image-code-err").hide();
     });
-    $("#phonecode").focus(function(){
+    $("#phonecode").focus(function () {
         $("#phone-code-err").hide();
     });
-    $("#password").focus(function(){
+    $("#password").focus(function () {
         $("#password-err").hide();
         $("#password2-err").hide();
     });
-    $("#password2").focus(function(){
+    $("#password2").focus(function () {
         $("#password2-err").hide();
     });
-    $(".form-register").submit(function(e){
+    $(".form-register").submit(function (e) {
         e.preventDefault();
         mobile = $("#mobile").val();
         phoneCode = $("#phonecode").val();
@@ -91,7 +99,7 @@ $(document).ready(function() {
             $("#mobile-err span").html("请填写正确的手机号！");
             $("#mobile-err").show();
             return;
-        } 
+        }
         if (!phoneCode) {
             $("#phone-code-err span").html("请填写短信验证码！");
             $("#phone-code-err").show();
