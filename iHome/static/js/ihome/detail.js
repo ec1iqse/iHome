059737fac9ea46e1fd7a90927383c1b3aa8f81af
User <admin@admin.com>
2020-04-20 22:28:03 +0800

    celery存在问题

--- iHome/static/js/ihome/detail.js
+++ iHome/static/js/ihome/detail.js
@@ -1,31 +1,34 @@
 function hrefBack() {
     history.go(-1);
 }
+
 // 解析提取url中的查询字符串参数
-function decodeQuery() {
+function decodeQuery(){
     let search = decodeURI(document.location.search);
-    return search.replace(/(^\?)/, '').split('&').reduce(function (result, item) {
+    return search.replace(/(^\?)/, '').split('&').reduce(function(result, item){
         values = item.split('=');
         result[values[0]] = values[1];
         return result;
     }, {});
 }
 
-$(document).ready(function () {
+$(document).ready(function(){
+    // 获取详情页面要展示的房屋编号
+    let queryData = decodeQuery();
+    let houseId = queryData["id"];
 
-    let queryData=decodeQuery();
-    let houseId=queryData["id"];
+    // 获取该房屋的详细信息
+    $.get("/api/v1.0/houses/" + houseId, function(resp){
+        if ("0" === resp.errno) {
+            $(".swiper-container").html(template("house-image-tmpl", {img_urls:resp.data.house.img_urls, price:resp.data.house.price}));
+            $(".detail-con").html(template("house-detail-tmpl", {house:resp.data.house}));
 
-    $.get("/api/v1.0/houses/"+houseId,function(resp){
-        if("0"===resp.errno){
-            $(".swiper-container").html(template("house-image-tmpl",{img_url:resp.data.house.im}));
-            $(".detail-con").html(template("house-image-tmpl",{house:resp.data.house}));
-            if (resp.data.user_id!=resp.data.house.user_id){
-                $(".book-house").attr("href","/booking.html+hid="resp.data.house.hid);
+            // resp.user_id为访问页面用户,resp.data.user_id为房东
+            if (resp.data.user_id != resp.data.house.user_id) {
+                $(".book-house").attr("href", "/booking.html?hid="+resp.data.house.hid);
                 $(".book-house").show();
-            };
-
-            let mySwiper = new Swiper('.swiper-container', {
+            }
+            let mySwiper = new Swiper ('.swiper-container', {
                 loop: true,
                 autoplay: 2000,
                 autoplayDisableOnInteraction: false,
@@ -33,5 +36,5 @@ $(document).ready(function () {
                 paginationType: 'fraction'
             });
         }
-    });
-});
+    })
+})
